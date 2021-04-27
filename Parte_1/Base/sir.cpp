39d96f1b4d4eee9e863de10728dae675a7c86340
Cristina Caprioglio <caprioglio.cristina.01@gmail.com>
2021-04-27 18:13:47 +0200

    Assert

--- Parte_1/Base/sir.cpp
+++ Parte_1/Base/sir.cpp
@@ -14,6 +14,7 @@ Population const Evolve(Population const& initial_population)
   next.S()=static_cast<int>(initial_population.S()-beta*(initial_population.S()/N)*initial_population.I());
   next.I()=static_cast<int>(initial_population.I()+beta*(initial_population.S()/N)*initial_population.I()-gamma*initial_population.I());
   next.R()=static_cast<int>(initial_population.R()+gamma*initial_population.I());
+  assert(next.S()+next.R()+next.I()==N);
   return next;
 }
 

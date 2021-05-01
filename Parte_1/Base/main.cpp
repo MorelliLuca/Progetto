3e30bf754e75b281d9af2cbbef7886c89cf47517
LucaMorelliUni <luca.morelli12@studio.unibo.it>
2021-05-01 18:42:44 +0200

    Added Part_2

--- Parte_1/Base/main.cpp
+++ Parte_1/Base/main.cpp
@@ -2,13 +2,22 @@
 #include <vector>
 #include "sir.hpp"
 
+
+Simulation::Population get_parameter(){  //Funzione che prende in input i parametri iniziali della simulazione
+double beta, gamma;
+Simulation::Data initial_state;
+std::cin >> beta >> gamma >> initial_state.S >> initial_state.I >> initial_state.R;
+Simulation::Population population{beta,gamma,initial_state};
+return population;
+}
+
+
 int main()
 {
-  double beta, gamma;
-  Simulation::Data initial_state;
+  Simulation::Population initial_population{get_parameter()};
   int simulation_t;
-  std::cin >> beta >> gamma >> initial_state.S >> initial_state.I >> initial_state.R >> simulation_t;
-  Simulation::Population initial_population{beta, gamma, initial_state};
+  std::cin>>simulation_t;
   std::vector<Simulation::Population> population{Simulation::Simulate(simulation_t, initial_population)};
   Simulation::Print(population);
+  return 0;
 }

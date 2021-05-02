fcf472412c60fbb69b2890e66acfef8830148b6d
LucaMorelliUni <luca.morelli12@studio.unibo.it>
2021-05-02 15:21:28 +0200

    Fixed exceptions

--- Parte_1/Base/main.cpp
+++ Parte_1/Base/main.cpp
@@ -1,64 +1,50 @@
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 #include "sir.hpp"
-#include <stdexcept>
-using namespace std;
+#include <limits>
 
-
-Simulation::Population get_parameter(){  //Funzione che prende in input i parametri iniziali della simulazione
-double beta, gamma;
-Simulation::Data initial_state;
-while(2){
-    std::cin >> beta >> gamma >> initial_state.S >> initial_state.I >> initial_state.R;
-  if (initial_state.S<0||initial_state.I<0||initial_state.R<0||beta<0||gamma<0)
-   {
-    throw std::runtime_error{"These parameters can't be less than 0"};
-   }
-  if (initial_state.S==0&&initial_state.I==0&&initial_state.R==0)
-   {
-    throw std::runtime_error{"These parameters can't all be 0"};
-   }
-   if (cin.fail())
-   {
-     throw std::runtime_error{"These parameters have to be numbers"};
-     cin.clear();
-     cin.ignore();
-   }
-   else {break;}
-}
-Simulation::Population population{beta,gamma,initial_state};
+Simulation::Population get_parameter()
+{  // Funzione che prende in input i parametri iniziali della simulazione
+  double beta, gamma;
+  Simulation::Data initial_state;
+  std::cin >> beta >> gamma >> initial_state.S >> initial_state.I >> initial_state.R;
+  if (initial_state.S < 0 || initial_state.I < 0 || initial_state.R < 0 || beta < 0 || gamma < 0) {
+    throw std::invalid_argument{"These parameters can't be less than 0"};
+  }
+  
+  if (std::cin.fail()) {
+    throw std::invalid_argument{"These parameters have to be numbers"};
+  }
+  if (initial_state.S == 0 && initial_state.I == 0 && initial_state.R == 0) { throw std::invalid_argument{"These parameters can't all be 0"}; }
+Simulation::Population population{beta, gamma, initial_state};
 return population;
 }
 
-
 int main()
 {
-  try {
-  Simulation::Population initial_population{get_parameter()};
-  int simulation_t;
-  while(2){
-    std::cin>>simulation_t;
-  if (simulation_t<=0)
-  {
-    throw std::runtime_error{"Time has to be more than 0"};
-  }
-  if (cin.fail())
-   {
-     throw std::runtime_error{"These parameters have to be numbers"};
-     cin.clear();
-     cin.ignore();
-   }
-   else {break;}
-  }
-  std::vector<Simulation::Population> population{Simulation::Simulate(simulation_t, initial_population)};
-  Simulation::Print(population);
-  }
-  catch (std::runtime_error const& e){
-    std::cerr << e.what() << '\n';
-  }
-  catch (...) {
-    std::cerr << "Caught unknown exception" <<'\n';
-    return EXIT_FAILURE;
+  while (1) {
+    try {
+      Simulation::Population initial_population{get_parameter()};
+      int simulation_t;
+      std::cin >> simulation_t;
+      if (simulation_t <= 0) { throw std::invalid_argument{"Time has to be more than 0"}; }
+      if (std::cin.fail()) {
+        throw std::invalid_argument{"These parameters have to be numbers"};
+        
+      }
+
+      std::vector<Simulation::Population> population{Simulation::Simulate(simulation_t, initial_population)};
+      Simulation::Print(population);
+      break;
+    } catch (std::invalid_argument const& e) {
+      std::cerr <<"\033[31mInavalid input:\033[0m "<< e.what() <<'\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } catch (...) {
+      std::cerr << "Caught unknown exception" << '\n';
+      return EXIT_FAILURE;
+    }
   }
   return 0;
 }

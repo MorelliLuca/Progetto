727f51cfe6dce4dc0b3e9d7ee5a3751b5a4313c1
LucaMorelliUni <luca.morelli12@studio.unibo.it>
2021-05-11 18:23:11 +0200

    fixed bug

--- Parte_2/Base/graph.hpp
+++ Parte_2/Base/graph.hpp
@@ -1,15 +1,15 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
-#include "plague.hpp"
 #include <SFML/Graphics.hpp>
+#include "plague.hpp"
 
 namespace Display {
-double person_size(sf::RenderWindow const &window, Simulation::World const &world);
+double person_size(sf::RenderWindow const& window, Simulation::World const& world);
 
-void print(sf::RenderWindow &window, Simulation::World const &world);
+void print(sf::RenderWindow& window, Simulation::World const& world);
 
-void setStatus(sf::RenderWindow &window, Simulation::World &world);
+void setStatus(sf::RenderWindow& window, Simulation::World& world);
 
-}
+}  // namespace Display
 
 #endif
--- Parte_2/Base/plague.hpp
+++ Parte_2/Base/plague.hpp
@@ -4,11 +4,12 @@
 #include <cassert>
 #include <vector>
 
-namespace Simulation{
+namespace Simulation {
 
-enum class Person { S,I,R };
+enum class Person { S, I, R };
 
-class World {
+class World
+{
   using Grid = std::vector<Person>;
   int side;
   Grid grid;
@@ -16,64 +17,55 @@ class World {
   double gamma;
   inline static constexpr Person removed_person = Person::R;
 
-public:
-  World(int N,double b, double g) : side{N}, grid(N * N, Person::S),beta{b},gamma{g}  {
-
-      assert(beta>=0);
-      assert(gamma>=0);
-      assert(N>0);
-
+ public:
+  World(int N, double b, double g) : side{N}, grid(N * N, Person::S), beta{b}, gamma{g}
+  {
+    assert(beta >= 0);
+    assert(gamma >= 0);
+    assert(N > 0);
   }
 
-  Person const &person(int r, int c) const {
+  Person const& person(int r, int c) const
+  {
     assert(r <= side && c <= side);
     assert(r >= -1 && c >= -1);
-    if (r == -1 || c == -1 || r == side || c == side) {
-      return removed_person;
-    }
+    if (r == -1 || c == -1 || r == side || c == side) { return removed_person; }
     int cursor = r * side + c;
     return grid[cursor];
   }
 
-  Person &person(int r, int c) {
+  Person& person(int r, int c)
+  {
     assert(r < side && c < side);
     assert(r >= 0 && c >= 0);
     int cursor = r * side + c;
     return grid[cursor];
   }
   int get_side() const { return side; }
-  double const& get_beta() const {return beta;}
-  double const& get_gamma() const {return gamma;}
-  
-  
+  double const& get_beta() const { return beta; }
+  double const& get_gamma() const { return gamma; }
 };
 
-inline bool operator==(World const &left, World const &right) {
+inline bool operator==(World const& left, World const& right)
+{
   bool result;
   for (int i{0}; i < left.get_side(); ++i) {
     for (int j{0}; j < left.get_side(); ++j) {
       result = left.person(i, j) == right.person(i, j);
-      if (result == false) {
-        return result;
-      }
+      if (result == false) { return result; }
     }
   }
   return result;
 }
 
-inline bool operator!=(World const &left, World const &right) {
-  return !(left == right);
-}
+inline bool operator!=(World const& left, World const& right) { return !(left == right); }
 
 inline int I_near(World const& world, int r, int c);
 
-inline Person check_next_status (World const& world, int r, int c);
+inline Person check_next_status(World const& world, int r, int c);
 
-World evolve (World const& current_world);
+World evolve(World const& current_world);
 
-
-}
-
-
+}  // namespace Simulation
 
 #endif

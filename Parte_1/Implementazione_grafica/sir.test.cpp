74d29067f0122356a1351237f50c2c59c07f8adc
Cristina Caprioglio <caprioglio.cristina.01@gmail.com>
2021-06-18 18:33:21 +0200

    Fixed tests

--- Parte_1/Implementazione_grafica/sir.test.cpp
+++ Parte_1/Implementazione_grafica/sir.test.cpp
@@ -25,7 +25,57 @@ TEST_CASE("Testing Class Population")
   CHECK(population.Beta() == .1);
   CHECK(population.Gamma() == .2);
 }
-
+TEST_CASE("Testing evolve function")
+{
+  SUBCASE("Beta>Gamma&&Quarantine")
+  {
+    Simulation::Data data{7000, 8000, 1000};
+    Simulation::Population population{0.1, .01, data, "No", 0};
+    Simulation::Population next = Simulation::Evolve(population);
+    CHECK(next.Total() == 16000);
+    CHECK(next.Beta() == .05);
+    CHECK(next.Gamma() == .01);
+    CHECK(next.S() == 6825);
+    CHECK(next.I() == 8095);
+    CHECK(next.R() == 1080);
+  }
+  SUBCASE("Beta<Gamma")
+  {
+    Simulation::Data data{7000, 8000, 1000};
+    Simulation::Population population{.03, .3, data, "No", 0};
+    Simulation::Population next = Simulation::Evolve(population);
+    CHECK(next.Total() == 16000);
+    CHECK(next.Beta() == .015);
+    CHECK(next.Gamma() == .3);
+    CHECK(next.S() == 6947);
+    CHECK(next.I() == 5653);
+    CHECK(next.R() == 3400);
+  }
+  SUBCASE("N~S && Beta>Gamma")
+  {
+    Simulation::Data data{7000, 10, 0};
+    Simulation::Population population{.3, .03, data, "No", 0};
+    Simulation::Population next = Simulation::Evolve(population);
+    CHECK(next.Total() == 7010);
+    CHECK(next.Beta() == .3);
+    CHECK(next.Gamma() == .03);
+    CHECK(next.S() == 6997);
+    CHECK(next.I() == 13);
+    CHECK(next.R() == 0);
+  }
+  SUBCASE("N~S && Beta<Gamma")
+  {
+    Simulation::Data data{7000, 10, 0};
+    Simulation::Population population{.03, .3, data, "No", 0};
+    Simulation::Population next = Simulation::Evolve(population);
+    CHECK(next.Total() == 7010);
+    CHECK(next.Beta() == .03);
+    CHECK(next.Gamma() == .3);
+    CHECK(next.S() == 7000);
+    CHECK(next.I() == 7);
+    CHECK(next.R() == 3);
+  }
+}
 TEST_CASE("Testing Simulate Function with quarantine")
 {
   Simulation::Data data{7000, 8000, 1000};
